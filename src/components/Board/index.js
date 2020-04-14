541a2559a86ebcda1f24c7f15094c3fa29472b41
lLink01 <39887414+lLink01@users.noreply.github.com>
2020-04-14 15:11:51 -0300

    React Hooks

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Square from "../Square";
 import { Container } from "./style";
 import findaWinner from "../../services/findaWinner";
@@ -6,17 +6,30 @@ export default () =>{
     const [board, setBoard] = useState(Array(9).fill(null))
     const [isPlayer1, setIsPlayer1] = useState(true);
     const [status, setStatus] = useState(`Próximo jogador: ${isPlayer1 ?'X': 'O'}`);
+    useEffect(()=>{
+        const winner = findaWinner(board);
+        if(winner){
+            setStatus(`Vencedor: ${winner}`);
+        }
+        else if(board.every((v) => v )){
+            setStatus(`Empate`);
+        }
+        else{
+            setStatus(`Próximo jogador: ${isPlayer1 ?'X': 'O'}`);
+        }
+    }, [board, isPlayer1])
     const renderSquare = (i) => {
         return (<Square value={board[i]}
             onClick={() => handleClick(i)} />);
     };
     const handleClick = (i) =>{
         const new_square = board.slice();
+        if (new_square[i] || findaWinner(board)) {
+            return null;
+        }
+        setIsPlayer1(!isPlayer1);
         new_square[i] = isPlayer1 ?'X': 'O';
         setBoard(new_square);
-        const winner = findaWinner(new_square);
-        
-         winner ? setStatus(`Vencedor: ${winner}`) : setIsPlayer1(!isPlayer1)
     }
     return (
         <Container>
